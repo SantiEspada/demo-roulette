06cdd3c78dae63edb5c1041627231bb248ad8235
Santiago Espada Escolar <santiago.espada@soprasteria.com>
2019-05-08 17:19:27 +0200

    fix: fixed builds

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,30 @@
 import createDefaultConfig from '@open-wc/building-rollup/modern-config';
+import minifyHTML from 'rollup-plugin-minify-html-literals';
+
 import copy from 'rollup-plugin-copy';
 
 const config = createDefaultConfig({ input: './src/index.html' });
+const production = !process.env.ROLLUP_WATCH;
+
+const basePlugins = production ? [...config.plugins.slice(1)] : [...config.plugins]
 
 export default {
   ...config,
   plugins: [
-    ...config.plugins,
+    ...basePlugins,
+    production && 
+      minifyHTML({
+        failOnError: true,
+        removeAttributeQuotes: false,
+      }),
     copy({
-      targets: [ 'assets '],
-      outputFolder: 'dist'
+      targets: {
+        'assets': 'dist/assets',
+        'favicon.ico': 'dist/favicon.ico',
+        'site.webmanifest': 'dist/site.webmanifest',
+        'browserconfig.xml': 'dist/browserconfig.xml',
+        'src/app.css': 'dist/app.css'
+      }
     })
   ]
 };
